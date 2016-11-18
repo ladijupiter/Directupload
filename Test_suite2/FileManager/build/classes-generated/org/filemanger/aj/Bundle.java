070d350ffb31284e5b4f19264a941c8172462de8
ladijupiter <hgallion@student.umuc.edu>
2016-11-17 20:24:31 -0500

    uploaded project

---
+++ Test_suite2/FileManager/build/classes-generated/org/filemanger/aj/Bundle.java
@@ -0,0 +1,27 @@
+package org.filemanger.aj;
+/** Localizable strings for {@link org.filemanger.aj}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Source</i>
+     * @see aspectFileTypeDataObject
+     */
+    static String LBL_aspectFileType_EDITOR() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_aspectFileType_EDITOR");
+    }
+    /**
+     * @return <i>Files of aspectFileType</i>
+     * @see aspectFileTypeDataObject
+     */
+    static String LBL_aspectFileType_LOADER() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_aspectFileType_LOADER");
+    }
+    /**
+     * @return <i>Visual</i>
+     * @see aspectFileTypeVisualElement
+     */
+    static String LBL_aspectFileType_VISUAL() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_aspectFileType_VISUAL");
+    }
+    private Bundle() {}
+}
